4d2edefa0562345d8086a4bfdf36916aa0bfe379
shima8823 <taiyaki1275@gmail.com>
2023-04-27 10:57:30 +0900

    旗とポールのテクスチャを追加

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -73,7 +73,10 @@ gTextures = {
     ['gems'] = love.graphics.newImage('graphics/gems.png'),
     ['backgrounds'] = love.graphics.newImage('graphics/backgrounds.png'),
     ['green-alien'] = love.graphics.newImage('graphics/green_alien.png'),
-    ['creatures'] = love.graphics.newImage('graphics/creatures.png')
+    ['creatures'] = love.graphics.newImage('graphics/creatures.png'),
+    ['keys-and-locks'] = love.graphics.newImage('graphics/keys_and_locks.png'),
+    ['flags'] = love.graphics.newImage('graphics/flags.png'),
+    ['poles'] = love.graphics.newImage('graphics/flags.png'),
 }
 
 gFrames = {
@@ -86,7 +89,10 @@ gFrames = {
     ['gems'] = GenerateQuads(gTextures['gems'], 16, 16),
     ['backgrounds'] = GenerateQuads(gTextures['backgrounds'], 256, 128),
     ['green-alien'] = GenerateQuads(gTextures['green-alien'], 16, 20),
-    ['creatures'] = GenerateQuads(gTextures['creatures'], 16, 16)
+    ['creatures'] = GenerateQuads(gTextures['creatures'], 16, 16),
+    ['keys-and-locks'] = GenerateQuads(gTextures['keys-and-locks'], 16, 16),
+    ['flags'] = GenerateFlags(gTextures['flags'], 3, 4),
+    ['poles'] = GeneratePoles(gTextures['poles'], 4),
 }
 
 -- these need to be added after gFrames is initialized because they refer to gFrames from within
